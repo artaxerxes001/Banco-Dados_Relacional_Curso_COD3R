3ddace4f7c699db7c7a9e8791dbfdeb7c934f1fc
artaxerxes <artaxerxesnazare@gmail.com>
2022-03-21 10:00:22 +0100

    inserindo cidades

---
+++ inserirCidades.sql
@@ -0,0 +1,14 @@
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Campinas', 795, 25)
+
+
+-- buscando o id e fazendo a inserção
+INSERT Into cidades (nome, area, estado_id)
+values ('Caruaru', 920.6 , 
+(select id FROM estados WHERE sigla = 'PE')
+)
+
+INSERT Into cidades (nome, area, estado_id)
+values ('Juazeiro do Norte', 248.2 , 
+(select id FROM estados WHERE sigla = 'CE')
+)
